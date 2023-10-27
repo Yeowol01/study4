6597d922a85850fa4de13371cf7002e81c7ffc92
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-10-27 19:47:50 +0900

    (2023.10.27) 벡터

--- DataStruct/CircleQueue.h
+++ DataStruct/CircleQueue.h
@@ -88,3 +88,26 @@ public:
 
 	~CircleQueue() {}
 };
+
+int main()
+{
+#pragma region 원형 큐
+	// 물리적으로는 선형 구조를 가지고 있으며, 큐의 시작점과 끝점을 연결한 큐입니다.
+
+	/*
+	CircleQueue<int> circleQueue;
+
+	circleQueue.EnQueue(100);
+	circleQueue.EnQueue(200);
+	circleQueue.EnQueue(300);
+
+	cout << circleQueue.Front() << endl;
+	cout << circleQueue.Back() << endl;
+
+	circleQueue.DeQueue();
+	circleQueue.DeQueue();
+	circleQueue.DeQueue();
+	*/
+
+#pragma endregion
+}
--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,26 +1,8 @@
 ﻿#include <iostream>
-#include "CircleQueue.h"
 
 using namespace std;
 
 int main()
 {
-#pragma region 원형 큐
-	// 물리적으로는 선형 구조를 가지고 있으며, 큐의 시작점과 끝점을 연결한 큐입니다.
-
-	CircleQueue<int> circleQueue;
-
-	circleQueue.EnQueue(100);
-	circleQueue.EnQueue(200);
-	circleQueue.EnQueue(300);
-
-	cout << circleQueue.Front() << endl;
-	cout << circleQueue.Back() << endl;
-
-	circleQueue.DeQueue();
-	circleQueue.DeQueue();
-	circleQueue.DeQueue();
-
-#pragma endregion
-
+	
 }
