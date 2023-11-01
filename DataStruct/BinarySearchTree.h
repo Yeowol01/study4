5ac069cdc31bcb698ec671c07884607f88b999b3
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-11-01 19:49:16 +0900

    Update BinarySearchTree.h

--- DataStruct/BinarySearchTree.h
+++ DataStruct/BinarySearchTree.h
@@ -20,9 +20,9 @@ private :
 		Node* right;
 	};
 
-	Node* rootNode;
-
 public : 
+
+	Node* rootNode;
 
 	BinarySearchTree()
 	{
@@ -34,14 +34,101 @@ public :
 		return rootNode;
 	}
 
-	void Insert(T data, Node* root)
+	Node * Insert(T data, Node* root)
 	{
+		if (Find(root, data) == true)
+		{
+			cout << "Duplicate Key" << endl;
+			return root;
+		}
+
 		if (root == nullptr)
 		{
 			root = new Node;
 			root->data = data;
 			root->left = nullptr;
 			root->right = nullptr;
+		}
+		else if (root->data > data) // else if root의 data보다 작다면?
+		{
+			root->left = Insert(data, root->left);
+		}
+		else if (root->data < data)
+		{
+			root->right = Insert(data, root->right);
+		}
+
+		return root;
+	}
+
+	bool Find(Node* root, T data)
+	{
+		if (root == nullptr)
+		{
+			return false;
+		}
+
+		if (root->data == data)
+		{
+			return true;
+		}
+		else
+		{
+			if (root->data > data)
+			{
+				return Find(root->left, data);
+			}
+			else
+			{
+				return Find(root->right, data);
+			}
+		}
+	}
+
+	T & FindMax(Node* root)
+	{
+		if (root == nullptr)
+		{
+			cout << "Binaryu Search Tree is Empty" << endl;
+			exit(1);
+		}
+
+		if (root->right == nullptr)
+		{
+			return root->data;
+		}
+		else
+		{
+			return FindMax(root->right);
+		}
+
+	}
+
+	T & FindMin(Node* root)
+	{
+		if (root == nullptr)
+		{
+			cout << "Binaryu Search Tree is Empty" << endl;
+			exit(1);
+		}
+
+		if (root->left == nullptr)
+		{
+			return root->data;
+		}
+		else
+		{
+			return Findmin(root->left);
+		}
+	}
+
+	void Inorder(Node* root)
+	{
+		if (root != nullptr)
+		{
+			Inorder(root->left);
+			cout << root->data << " ";
+			Inorder(root->right);
 		}
 	}
 };
